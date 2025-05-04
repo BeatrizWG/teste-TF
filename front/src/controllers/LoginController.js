a9ab259e28276dbf499c6759a3902326f84bdb01
Beatriz Wanderley <143552212+BeatrizWG@users.noreply.github.com>
2025-05-04 14:26:39 -0300

    Update LoginController.js

--- front/src/controllers/LoginController.js
+++ front/src/controllers/LoginController.js
@@ -14,7 +14,7 @@ export const useLoginController = () => {
     setErrorMessagePassword('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://teste-tf.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'

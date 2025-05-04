8763cde62c60fc4cc79eba3f3201c4a9594bed60
Beatriz Wanderley <143552212+BeatrizWG@users.noreply.github.com>
2025-05-04 14:26:10 -0300

    Update RegisterController.js

--- front/src/controllers/RegisterController.js
+++ front/src/controllers/RegisterController.js
@@ -22,7 +22,7 @@ export const useRegisterController = () => {
     setErrorMessageAccessCode('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://teste-tf.onrender.com/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
